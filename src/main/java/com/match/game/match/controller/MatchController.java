44f3039a9e806f837a8b66b30a15373030b3981a
KangMinGu <mincu1028@gmail.com>
2018-03-21 22:57:35 +0900

    팀 매칭 신청폼 컨트롤러 연결

--- src/main/java/com/match/game/match/controller/MatchController.java
+++ src/main/java/com/match/game/match/controller/MatchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +17,19 @@ public class MatchController {
     @Autowired
     private MatchService matchService;
 
-    //해당팀 대전신청하기
-    @RequestMapping(value = "/team/contents/match/vs/{num}", method = RequestMethod.GET)
+    //해당팀 대전 신청폼으로 이동
+    @RequestMapping(value = "/match/{num}", method = RequestMethod.GET)
     public ModelAndView vsForm(HttpServletRequest request, @PathVariable int num){
-        System.out.println("매치컨트롤러들어옴");
-        ModelAndView mView = new ModelAndView();
-        matchService.vsForm(request, num);
+        System.out.println("매치컨트롤러들어옴"+num);
+        ModelAndView mView = matchService.vsForm(request,num);
         mView.setViewName("match/vs");
         return mView;
+    }
+
+    @RequestMapping(value = "/match/info/{teamname}", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView teamname(@PathVariable String teamname){
+        System.out.println("아작스팀"+teamname);
+        return null;
     }
 }
